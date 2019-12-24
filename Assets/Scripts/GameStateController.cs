13fd497c67af0ce1d67f48c4a8a590e6ab087e3f
marth8880 <marth8880@gmail.com>
2019-12-24 11:27:37 -0800

    Game state controller

---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
