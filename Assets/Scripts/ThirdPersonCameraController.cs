c959ca62766900c94601600dce3f0a7a081420e2
marth8880 <marth8880@gmail.com>
2019-12-24 15:23:04 -0800

    Character yaw rotation

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    // Entity properties
+    public float LookYawSpeedMultiplier = 1.0f;
+
+    void Start()
+    {
+        camera = gameObject.GetComponentInChildren<Camera>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * LookYawSpeedMultiplier);
+    }
+}
