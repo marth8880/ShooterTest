9c9089181ec5d5ae28cabda6731f0f7919b4d95c
marth8880 <marth8880@gmail.com>
2019-12-27 11:06:36 -0800

    Crosshair functionality

---
+++ Assets/Scripts/HUD/HUDController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    public Crosshair CrosshairPrefab;
+
+    Crosshair crosshairInstance;
+
+    void Start()
+    {
+        crosshairInstance = Instantiate(CrosshairPrefab);
+    }
+
+    void Update()
+    {
+        
+    }
+}
