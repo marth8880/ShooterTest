5efb8ea0f5348a4d1bff884d12deb6b477c0b587
marth8880 <marth8880@gmail.com>
2020-01-17 23:00:06 +0000

    Movement refactor progress

--- Assets/Scripts/Character/CharacterMovementLogic.cs
+++ Assets/Scripts/Character/CharacterMovementLogic.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class CharacterMovementLogic : MonoBehaviour
 {
     // Entity properties
@@ -40,37 +42,37 @@ public class CharacterMovementLogic : MonoBehaviour
     {
         if (!gameStateController.isDebugMode && gameStateController.CurrentGameState == GameStateController.GameState.Gameplay)
         {
-            if (Input.GetButton("MoveForward"))
-            {
-                if (grounded)
-                    Move(new Vector3(0, 0, MovementSpeed));
-                else
-                    Move(new Vector3(0, 0, airMovementSpeed));
-            }
+            //if (Input.GetButton("MoveForward"))
+            //{
+            //    if (grounded)
+            //        Move(new Vector3(0, 0, MovementSpeed));
+            //    else
+            //        Move(new Vector3(0, 0, airMovementSpeed));
+            //}
 
-            if (Input.GetButton("MoveBackward"))
-            {
-                if (grounded)
-                    Move(new Vector3(0, 0, -MovementSpeed));
-                else
-                    Move(new Vector3(0, 0, -airMovementSpeed));
-            }
+            //if (Input.GetButton("MoveBackward"))
+            //{
+            //    if (grounded)
+            //        Move(new Vector3(0, 0, -MovementSpeed));
+            //    else
+            //        Move(new Vector3(0, 0, -airMovementSpeed));
+            //}
 
-            if (Input.GetButton("MoveLeft"))
-            {
-                if (grounded)
-                    Move(new Vector3(-MovementSpeed, 0, 0));
-                else
-                    Move(new Vector3(-airMovementSpeed, 0, 0));
-            }
+            //if (Input.GetButton("MoveLeft"))
+            //{
+            //    if (grounded)
+            //        Move(new Vector3(-MovementSpeed, 0, 0));
+            //    else
+            //        Move(new Vector3(-airMovementSpeed, 0, 0));
+            //}
 
-            if (Input.GetButton("MoveRight"))
-            {
-                if (grounded)
-                    Move(new Vector3(MovementSpeed, 0, 0));
-                else
-                    Move(new Vector3(airMovementSpeed, 0, 0));
-            }
+            //if (Input.GetButton("MoveRight"))
+            //{
+            //    if (grounded)
+            //        Move(new Vector3(MovementSpeed, 0, 0));
+            //    else
+            //        Move(new Vector3(airMovementSpeed, 0, 0));
+            //}
 
             if (Input.GetButtonDown("Jump") && grounded == true)
             {
@@ -85,6 +87,15 @@ public class CharacterMovementLogic : MonoBehaviour
         grounded = true;
     }
 
+    public void OnMove(InputValue value)
+    {
+        Vector2 direction = value.Get<Vector2>();
+
+        if (grounded)
+            Move(new Vector3(direction.x * MovementSpeed, 0, direction.y * MovementSpeed));
+        else
+            Move(new Vector3(direction.x * airMovementSpeed, 0, direction.y * airMovementSpeed));
+    }
 
     void Move(Vector3 direction)
     {
